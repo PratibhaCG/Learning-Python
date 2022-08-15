d431351c500c377902a055e91a4acb6a43be1072
Pratibha G <pratibhachowdary@gmail.com>
2022-08-15 19:50:39 +1000

    Calculator in Python

---
+++ Calculator.py
@@ -0,0 +1,60 @@
+# Options to choose from
+print("Choose an option from the following:")
+print("(1) Add(+)")
+print("(2) Subtract(-)")
+print("(3) Multiply(*)")
+print("(4) Divide(/)")
+print("(5) Find Remainder(%)")
+print("(6) Find Quotient(//)")
+print("(7) Find Square of a number(**)")
+
+# Enter an option for calculation
+c = int(input("Option selected from 1 to 7: "))
+
+# Enter first number
+a = int(input("Enter 1st number: "))
+
+# Enter second number
+b = int(input("Enter 2nd number: "))
+
+# If elif else condition to create the calculator
+# Addition
+if c == 1:
+    print("Output is: ")
+    d = a+b
+    print(d)
+
+# Subtraction
+elif c == 2:
+    print("Output is: ")
+    d = a-b
+    print(d)
+
+# Multiplication
+elif c == 3:
+    print("Output is: ")
+    d = a*b
+    print(d)
+
+# Division
+elif c == 4:
+    print("Output is: ")
+    d = a/b
+    print(d)
+
+# Find the remainder
+elif c == 5:
+    print("Output is: ")
+    d = a%b
+    print(d)
+
+# Find the quotient
+elif c == 6:
+    print("Output is: ")
+    d = a//b
+    print(d)
+
+# Find the square root
+else:
+    d = a**b
+    print(d)
