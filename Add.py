7d1d8f1dcc92008486fb8d32d6f53d8423c3ca96
Pratibha G <pratibhachowdary@gmail.com>
2022-08-15 19:50:18 +1000

    Addition in Python

---
+++ Add.py
@@ -0,0 +1,19 @@
+# Enter first number
+a = int(input("Enter 1st number: "))
+
+# Enter second number
+b = int(input("Enter 2nd number: "))
+
+# If elif else condition
+if b < 0:
+    print('Denominator is negative')
+elif b == 0:
+    print('Enter a number that is not equal to zero')
+else:
+    c = a/b    print(c)
+
+
+
+
+
+
