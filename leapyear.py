8cc2a0dacb090b8f8f89abd01930d064116179e8
Pratibha G <pratibhachowdary@gmail.com>
2022-08-16 19:54:10 +1000

    Calculator in Python

---
+++ leapyear.py
@@ -0,0 +1,14 @@
+# Enter an Year for calculation
+a = int(input("Enter an Year: "))
+print("Checking if", a, "is a Leap Year")
+
+# Condition to check if Leap Year
+if a%4 == 0:
+    print (a, "is a Leap Year")
+
+else:
+    print (a, "is not a Leap Year")
+
+
+
+
