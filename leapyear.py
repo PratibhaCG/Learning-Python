edafe95dc7d334a1c74c883c71dda98bca69a04f
Pratibha G <pratibhachowdary@gmail.com>
2022-08-18 19:28:41 +1000

    Calculator in Python

--- leapyear.py
+++ leapyear.py
@@ -3,11 +3,15 @@ a = int(input("Enter an Year: "))
 print("Checking if", a, "is a Leap Year")
 
 # Condition to check if Leap Year
-if a%4 == 0:
-    print (a, "is a Leap Year")
+if a%400 == 0:
+    print(a, "is a Leap Year as it is divisible by 400.")
 
-else:
-    print (a, "is not a Leap Year")
+elif a%100 == 0:
+    print(a, "is not a Leap Year as it is divisible by 100 but not by 400.")
+
+elif a%4 == 0:
+    print (a, "is a Leap Year as it is divisible by 4 but not by 100 or 400.")
+
 
 
 
