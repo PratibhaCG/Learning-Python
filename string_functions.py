74e08c734785b34441aac3771576f3fd9b80d686
PratibhaCG <103414487+PratibhaCG@users.noreply.github.com>
2022-08-30 19:04:50 +1000

    Update string_functions.py
    Changes made as suggested by Leo

--- string_functions.py
+++ string_functions.py
@@ -5,28 +5,28 @@ a = input("Enter a string value: ")
 print(f"You've entered the string <{a}>")
 
 #Apply method lower to the string value
-b = str.lower(a)
+b = a.lower(a)
 
 #Print the output
 print(f"applying method lower() on string <{a}>")
 print(f"Result is: {b}")
 
 #Apply method upper to the string value
-c = str.upper(a)
+c = a.upper(a)
 
 #Print the output
 print(f"applying method upper() on string <{a}>")
 print(f"Result is: {c}")
 
 #Apply method title to the string value
-d = str.title(a)
+d = a.title(a)
 
 #Print the output
 print(f"applying method title() on string <{a}>")
 print(f"Result is: {d}")
 
 #Apply method swapcase to the string value
-e = str.swapcase(a)
+e = a.swapcase(a)
 
 #Print the output
 print(f"applying method swapcase() on string <{a}>")
