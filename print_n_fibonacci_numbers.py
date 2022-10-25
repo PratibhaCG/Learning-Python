ea9f4ab81ff1acc4382df788cff61cecb991cc0e
Pratibha G <pratibhachowdary@gmail.com>
2022-10-25 20:50:40 +1100

    Prints first n number of fibonacci series of numbers

--- print_n_fibonacci_numbers.py
+++ print_n_fibonacci_numbers.py
@@ -1,6 +1,12 @@
 # Enter a numeric value
-#a = int(input("Enter a number: "))
-#print(f"You've entered <{a}>")
+#a = int(input("Enter a positive integer: "))
+
+# Check if a is a positive integer
+
+
+#print(f"The fibonacci sequence will start from <{a}>")
+
+# Check if a is a fibonacci number else pick first fibonacci number after that - first loop and then once done, print the first number after a
 
 # Ask for the number of times to be printed
 b = int(input("How many fibonacci numbers you wish to print?\n"))
@@ -15,10 +21,13 @@ if b < 0:
     print("Incorrect input")
 
 elif b == 0:
-    print(0)
+    print("Input should be at least 1")
 
 elif b == 1:
-    print(1)
+    print(n0)
+
+elif b == 2:
+    print(n0,n1)
 
 #elif a > 1 and a <= 2
  #   print (2)
@@ -29,7 +38,8 @@ elif b == 1:
 
 
 else:
-   for i in range(b-1):
+    print(f"{n0} \n{n1}")
+    for i in range(b-2):
        n = n0 + n1
        n0 = n1
        n1 = n
